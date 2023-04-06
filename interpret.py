01559ff9f02eb7e758c928674bcdb28420e545cc
Msoukup2K <soukup-martin@email.cz>
2023-04-06 16:05:22 +0200

    arguments

--- interpret.py
+++ interpret.py
@@ -1,5 +1,6 @@
 import re
 import xml.etree.ElementTree as ET
+import fileinput
 import argparse
 import sys
 
@@ -23,10 +24,23 @@ class XMLInterpret:
 
 if __name__ == "__main__":
     
-    parser = argparse.ArgumentParser(description="Argument parser for interpret.py")
+    parser = argparse.ArgumentParser(description="Argument parser for interpret.py", add_help=False)
     
-    parser.add_argument("--help", help="Prints help for usage of interpret.py", default=sys.stdin, action="store")
-    print(parser)
-    print('HEEELP')
-        
-        
+    parser.add_argument("--help", help="Prints help for usage of interpret.py",
+                        action="help", )
+    parser.add_argument("--source", help="Source file with XML structure of parsed IppCode23",
+                        action="store")
+    parser.add_argument("--input", help="Input file with data to read",
+                        action="store")
+    args = parser.parse_args()
+
+    if not args.source and not args.input:
+        print("Pleas provide either input file or a source file")
+        sys.exit(52)
+
+    if not args.input:
+        print("input: ")
+
+    if not args.source:
+        print("sauce: ")
+
